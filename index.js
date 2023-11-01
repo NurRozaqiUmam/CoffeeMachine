403ec32b9b2996edd2b07a6950a5870e96814508
NurRozaqiUmam <107600122+NurRozaqiUmam@users.noreply.github.com>
2023-11-01 11:12:29 +0700

    add new func

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+// const {coffeeStock, isCoffeeMachineReady} = require('./state');
+
+// console.log(coffeeStock);
+// console.log(isCoffeeMachineReady);
+
+import { coffeeStock, isCoffeeMachineReady } from './state.js';
+
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
---
+++ state.js
@@ -0,0 +1,19 @@
+// const coffeeStock = {
+//     arabica: 100,
+//     robusta: 150,
+//     liberica: 200
+// }
+
+// const isCoffeeMachineReady = true;
+
+// module.exports = {coffeeStock, isCoffeeMachineReady};
+
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+};
+
+const isCoffeeMachineReady = true;
+
+export { coffeeStock, isCoffeeMachineReady };
